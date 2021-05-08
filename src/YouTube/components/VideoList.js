0658a731f266c89a0d5a8fc8f87ac517808b1183
akuto aoyama <aaaa.akuto@gmail.com>
2021-05-08 23:19:18 +0900

    [fix]YouTube関連動画の表示成功

--- src/YouTube/components/VideoList.js
+++ src/YouTube/components/VideoList.js
@@ -1,7 +1,6 @@
 import "../../assets/App.css";
-import React, { useState, useCallback } from "react";
-import { Button, CardColumns, Card, Form } from "react-bootstrap";
-import _ from "lodash";
+import React from "react";
+import { CardColumns, Card } from "react-bootstrap";
 
 export const VideoList = (props) => {
   return (
@@ -10,36 +9,37 @@ export const VideoList = (props) => {
         <div>loading...</div>
       ) : (
         <>
-          {props.videos.length !== 0 ? (
-            <CardColumns>
-              {props.videos.map((video) => (
-                <div key={video.id.videoId}>
-                  <Card className="border-none">
-                    {/* <div onClick={() => handleModalSubmit(image)} variant="light"> */}
-                    <div variant="light">
-                      <Card.Img
-                        variant="top"
-                        src={video.snippet.thumbnails.high.url}
-                      />
-                      <div>{video.snippet.title}</div>
-                      <a
-                        href={`https://www.youtube.com/embed/${video.id.videoId}`}
-                      >
-                        YouTubeのリンク（埋め込み）
-                      </a>
-                      <a
-                        href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
-                      >
-                        YouTubeのリンク
-                      </a>
-                    </div>
-                  </Card>
-                </div>
-              ))}
-            </CardColumns>
-          ) : (
-            <div>検索結果なし！もう一度ボタンをおしてください</div>
-          )}
+          {/* {props.videos.length !== 0 ? ( */}
+          <CardColumns>
+            {props.videos.map((video) => (
+              <div key={video.id.videoId}>
+                <Card className="border-none">
+                  {/* <div onClick={() => handleModalSubmit(image)} variant="light"> */}
+                  <div variant="light">
+                    <Card.Img
+                      variant="top"
+                      src={video.snippet.thumbnails.high.url}
+                    />
+                    <div>{video.snippet.title}</div>
+                    <a
+                      href={`https://www.youtube.com/embed/${video.id.videoId}`}
+                    >
+                      YouTubeのリンク（埋め込み）
+                    </a>
+                    <a
+                      href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
+                    >
+                      YouTubeのリンク
+                    </a>
+                  </div>
+                </Card>
+              </div>
+            ))}
+          </CardColumns>
+          {/* ) : (
+            <div>{videoExistText}</div>
+          )} */}
+          <div>{props.videoExistText}</div>
         </>
       )}
     </div>
--- src/YouTube/pages/YoutubeList.js
+++ src/YouTube/pages/YoutubeList.js
@@ -1,26 +1,19 @@
 import "../../assets/App.css";
 import React, { useState, useCallback } from "react";
 import { Button } from "react-bootstrap";
-import _ from "lodash";
+// import _ from "lodash";
 import { VideoList } from "../components/VideoList";
-// import { VerticallyCenteredModal } from "../components/VerticallyCenteredModal";
-// import { Toast } from "../components/Toast";
-// import { toast } from "react-toastify";
+
 const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 const YOUTUBE_SERACH_API_URI = "https://www.googleapis.com/youtube/v3/search?";
 
-const RANDOM_WIKI_API_URI =
-  // "https://ja.wikipedia.org/w/api.php?format=json&action=query&list=random&rnnamespace=0&rnlimit=1&origin=*";
-  "https://ja.wikipedia.org/w/api.php?";
+const RANDOM_WIKI_API_URI = "https://ja.wikipedia.org/w/api.php?";
 
 export const YoutubeList = () => {
   const [videos, setVideos] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [displayRandomWikiTitle, setDisplayRandomWikiTitle] = useState("");
-  // const [displayWikiExtract, setDisplayWikiExtract] = useState("");
-
-  // const [modalShow, setModalShow] = useState(false);
-  // const [clickedImage, setClickedImage] = useState(undefined);
+  const [videoExistText, setVideoExistText] = useState("");
 
   const getRandomWikiData = useCallback(async function () {
     const params = {
@@ -33,14 +26,9 @@ export const YoutubeList = () => {
       prop: "info",
     };
     const queryParams = new URLSearchParams(params);
-
     const res = await fetch(RANDOM_WIKI_API_URI + queryParams);
-    console.log("wikiURL", RANDOM_WIKI_API_URI + queryParams);
-
     const data = await res.json();
-    console.log("wikidata", data);
     const random_title = data.query.random[0].title;
-    console.log("getRandomWikiData内のrandom_title", random_title);
 
     return random_title;
   }, []);
@@ -50,23 +38,13 @@ export const YoutubeList = () => {
       format: "json",
       action: "query",
       titles: randomWikiTitle,
-      // titles: "フレディ・クルーガー",
-
       prop: "extracts",
-      // explaintext: "true", //HTMLではなくプレインテキストを返す
       redirects: "1",
       origin: "*",
     };
     const queryParams = new URLSearchParams(params);
-
     const res = await fetch(RANDOM_WIKI_API_URI + queryParams);
-    console.log(
-      "fetchWikiDataFromTitleのwikiURL",
-      RANDOM_WIKI_API_URI + queryParams
-    );
-
     const data = await res.json();
-    // debugger;
     const obj = data.query.pages;
     var wikiExtract = Object.keys(obj).map(function (key) {
       return obj[key];
@@ -76,13 +54,6 @@ export const YoutubeList = () => {
     var parent_element = document.getElementById("set-wiki-extract");
     parent_element.appendChild(wikiExtract);
 
-    console.log("fetchWikiDataFromTitleのwikiExtract", wikiExtract);
-
-    // setDisplayWikiExtract(wikiExtract);
-    console.log("fetchWikiDataFromTitleのdata", data);
-
-    // const random_title = data.query.random[0].title;
-
     return randomWikiTitle;
   }, []);
 
@@ -90,40 +61,36 @@ export const YoutubeList = () => {
     const params = {
       key: YOUTUBE_API_KEY,
       q: randomWikiTitle, // 検索キーワード
-      // type: "video", // video,channel,playlistから選択できる
       maxResults: "3", // 結果の最大数
       order: "viewCount", // 結果の並び順を再生回数の多い順に
       part: "snippet",
       videoType: "any",
     };
     const queryParams = new URLSearchParams(params);
-
     const res = await fetch(YOUTUBE_SERACH_API_URI + queryParams);
     const data = await res.json();
-    console.log("⑦getYouTubeVideos内のdata", data);
-    console.log("⑨getYouTubeVideos内のdata.items", data.items);
 
     return data.items;
   }, []);
 
   const onSubmit = useCallback(function () {
+    var parent_element = document.getElementById("set-wiki-extract");
+    var clone = parent_element.cloneNode(false); //ガワだけ複製して…
+    parent_element.parentNode.replaceChild(clone, parent_element); //すげ替え。
     console.log("onSubmit内上部");
 
     setIsLoading(true);
     getRandomWikiData().then(function (randomWikiTitle) {
-      var parent_element = document.getElementById("set-wiki-extract");
-      var clone = parent_element.cloneNode(false); //ガワだけ複製して…
-      parent_element.parentNode.replaceChild(clone, parent_element); //すげ替え。
-
-      console.log("getRandomWikiData後の", randomWikiTitle);
-
       fetchWikiDataFromTitle(randomWikiTitle).then(function (randomWikiTitle) {
-        console.log("fetchWikiDataFromTitle後の", randomWikiTitle);
-
         getYoutubeData(randomWikiTitle).then(function (result) {
           setDisplayRandomWikiTitle(randomWikiTitle);
           setVideos(result);
-          console.log("setVideos後のvideos", videos);
+
+          if (videos !== []) {
+            setVideoExistText("YouTube関連動画");
+          } else {
+            setVideoExistText("YouTube関連動画なし。");
+          }
 
           setIsLoading(false);
         });
@@ -163,12 +130,12 @@ export const YoutubeList = () => {
         </div>
         <div className="display-query text-2xl">{displayRandomWikiTitle}</div>
 
-        {/* <div id="set-wiki-extract" className="display-query text-2xl"></div> */}
-
-        {/* displayWikiExtract */}
-        {/* ------------------------- */}
         <div className="h-96">
-          <VideoList videos={videos} isLoading={isLoading} />
+          <VideoList
+            videos={videos}
+            isLoading={isLoading}
+            videoExistText={videoExistText}
+          />
         </div>
         {/* clickedImageの有無によって表示を分岐
 これによって、clickeexport const functionName = (params) => {
@@ -184,9 +151,7 @@ dImage内のhashでundefinedのエラーがでない */}
           <></>
         )} */}
         {/* <Toast /> */}
-        <div id="set-wiki-extract" className="display-query text-2xl">
-          {/* {displayWikiExtract} */}
-        </div>
+        <div id="set-wiki-extract" className="display-query text-2xl"></div>
       </div>
     </div>
   );
